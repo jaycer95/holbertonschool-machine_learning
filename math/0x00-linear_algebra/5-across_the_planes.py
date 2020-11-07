05cdcfa38a94d40f137ca05d1aa7f74451a0ef7b
jaycer95 <jacer.dabbabi95@gmail.com>
2020-11-07 23:47:09 +0100

    Task 5

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def add_matrices2D(mat1, mat2):
+    """ sum matrix """
+
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    m = [[x + y for x,y in zip(mat1[i], mat2[i])] for i in range(len(mat1))]
+    return m
