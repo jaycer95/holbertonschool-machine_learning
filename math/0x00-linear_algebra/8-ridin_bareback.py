2a723bf965f8c6791921c812b53bf24c48990400
jaycer95 <jacer.dabbabi95@gmail.com>
2020-11-08 02:11:17 +0100

    Task 8

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Multiply matrix"""
+
+
+def mat_mul(mat1, mat2):
+    if len(mat1[0]) != len(mat2):
+        return None
+    m = []
+    r = []
+    tr = [[row[i] for row in mat2] for i in range(len(mat2[0]))]
+    for i in mat1:
+        for j in tr:
+            r.append(sum([k * l for k, l in zip(i, j)]))
+        m.append(r)
+        r = []
+    return m
