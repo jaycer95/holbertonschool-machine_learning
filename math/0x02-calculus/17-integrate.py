cccf1d832c61d0a10e58cb7ce42b5c340525adcb
jaycer95 <jacer.dabbabi95@gmail.com>
2020-12-10 12:55:34 -0800

    fix

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -9,17 +9,18 @@ def is_whole(n):
 
 def poly_integral(poly, C=0):
     """ Calculate the integral of a polynomial """
-    list = [C]
-    if not isinstance(C, (int, float)) or not isinstance(poly, list) or poly == []:
+    lp = [C]
+    if not isinstance(C, (int, float))
+    or not isinstance(poly, list) or poly == []:
         return None
     for i in range(0, len(poly)):
         if type(poly[i]) is not int and type(poly[i]) is not float:
             return None
         try:
             if is_whole(poly[i] / (i + 1)):
-                list.append(int(poly[i] / (i + 1)))
+                lp.append(int(poly[i] / (i + 1)))
             else:
-                list.append(poly[i] / (i + 1))
+                lp.append(poly[i] / (i + 1))
         except ZeroDivisionError:
-            list.append(0)
-    return list
+            lp.append(0)
+    return lp
