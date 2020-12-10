7f497eec70235abc94ffd423aeff3340e4d59047
jaycer95 <jacer.dabbabi95@gmail.com>
2020-12-10 12:30:34 -0800

    fix

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,13 +4,13 @@
 
 def poly_derivative(poly):
     """   Calculate the derivative of a polynomial   """
-    list = []
-    if poly is None:
+    l = []
+    if type(poly) != l:
         return None
-    if len(poly) == 1:
+    if len(poly) <= 1:
         return [0]
     for i in range(1, len(poly)):
         if type(poly[i]) is not int and type(poly[i]) is not float:
             return None
-        list.append(i * poly[i])
-    return list
+        l.append(i * poly[i])
+    return l
