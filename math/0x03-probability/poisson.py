4f0d1825c86f0a9799e4ad4c8a1da936e079a3a8
jaycer95 <jacer.dabbabi95@gmail.com>
2020-12-15 07:16:49 -0800

    Poisson distribution

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -9,7 +9,7 @@ class Poisson:
         """ Class contructor """
         self.data = data
         if data is None:
-            if lambtha < 0:
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
