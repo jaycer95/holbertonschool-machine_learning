23cc76b4e647a9629911d3f35d39923bff79db3b
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-03 06:49:12 -0800

    4

--- supervised_learning/0x01-classification/4-neuron.py
+++ supervised_learning/0x01-classification/4-neuron.py
@@ -50,15 +50,6 @@ class Neuron:
         """ Evaluate the neuron’s predictions """
 
         a = self.forward_prop(X)
-        P = a.astype(int)
-        # P[P >= 0.5] = 1
-        # P[P < 0.5] = 0
-        m = X.shape[1]
-        P = np.zeros((1, m), dtype=int)
-        for i in range(a.shape[1]):
-            if a[0, i] >= 0.5:
-                P[0, i] = 1
-            else:
-                P[0, i] = 0
+        P = np.where(a < 0.5, 0, 1)
         c = self.cost(Y, a)
         return P, c
