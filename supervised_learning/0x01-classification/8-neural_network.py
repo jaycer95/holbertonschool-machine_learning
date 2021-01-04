02b1639cc9aa0e502cc6a0f2e2a61c61b5c94671
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-04 06:21:50 -0800

    Neural Network

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" NeuronNetwork Class """
+
+
+import numpy as np
+
+
+class NeuronNetwork:
+    """ Define a neural network with one hidden layer """
+    def __init__(self, nx, nodes):
+        """Instantination"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.nx = nx
+        self.nodes = nodes
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
