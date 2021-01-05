f5164c619c90f90551c153e8bb9282015c4790c5
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-05 12:57:18 -0800

    Initialization of Deep Neural Network

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" DeepNeuralNetwork Class """
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Define a Deep Neural Network performing binary classification """
+
+    def __init__(self, nx, layers):
+        """ Instantination """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or layers == []:
+            raise TypeError("layers must be a list of positive integers")
+        if not (layers > 0).all() and not all(
+                map(lambda x: isinstance(x, int), layers)):
+            raise TypeError("layers must be a list of positive integers")
+        self.L = 0
+        self.cache = {}
+        self.weights = {}
+        L = len(layers) - 1  # integer representing the number of layers
+        for l in range(1, L + 1):
+            self.weights["W" + str(l)] = np.random.randn(layers[l],
+                                                         nx) * np.sqrt(2 / nx)
+            self.weights["b" + str(l)] = np.zeros((layers[l], 1))
