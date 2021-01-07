450f8b8bc343d55eba1a252c172c5c90a4550bc9
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-07 06:40:32 -0800

    Decode

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """ One hot decode """
+    if len(one_hot.shape) != 2:
+        return None
     try:
         return np.argmax(one_hot.T, axis=1)
     except Exception:
