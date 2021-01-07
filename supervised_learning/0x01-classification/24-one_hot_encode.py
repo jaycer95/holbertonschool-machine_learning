e7d52ccb60147b3e7fdb0044841f50d1ade9e2a1
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-07 06:28:26 -0800

    encode

---
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """ One hot encode """
+    try:
+        return np.eye(classes)[Y.reshape(-1)].T
+    except Exception:
+        return None
