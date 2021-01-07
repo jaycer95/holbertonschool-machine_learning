bb49ccf4d699d03a41f639b39a5c518a5fbbbeef
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-07 12:18:51 -0800

    Test

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -6,16 +6,13 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """ calculates the accuracy of a prediction """
-    return tf.keras.backend.mean(
-        tf.keras.backend.equal(
-            tf.keras.backend.argmax(
-                y,
-                axis=-1),
-            tf.keras.backend.argmax(
-                y_pred,
-                axis=-1)))
-
-    # logits = tf.transpose(y_pred)
-    # labels = tf.transpose(y)
-    # return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits =
-    # logits , labels = labels))
+    return tf.math.reduce_mean(
+        tf.cast(
+            tf.math.equal(
+                tf.math.argmax(
+                    y,
+                    axis=-1),
+                tf.math.argmax(
+                    y_pred,
+                    axis=-1)),
+            tf.float32))
