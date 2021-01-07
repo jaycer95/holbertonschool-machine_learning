e0c88aa35c621ac9c5cebf853b108a2dd53ff186
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-07 06:31:29 -0800

    encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -5,7 +5,9 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ One hot encode """
+    if not isinstance(Y, np.ndarray):
+        return None
     try:
-        return np.eye(classes)[Y].T
+        return np.eye(classes)[Y.reshape(-1)].T
     except Exception:
         return None
