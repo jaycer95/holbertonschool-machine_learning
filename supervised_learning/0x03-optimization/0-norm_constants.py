04a7e4845f0efa3915dc566c3c47d85409e480ee
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-11 06:56:30 -0800

    Mean , STD

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Normalization function """
+
+import numpy as np
+
+
+def normalization_constants(X):
+    """ calculates the normalization (standardization) constants of a matrix """
+    
+    return np.mean(X, axis=0), np.std(X, axis=0)
