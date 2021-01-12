01bde726f3535a9c48a1acd79013b58b65a0c5cd
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-12 02:39:56 -0800

    Moving avg

---
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Normalization function """
+
+import numpy as np
+
+def moving_average(data, beta):
+    """ calculates the weighted moving average of a data set """
+    ema = []
+    avg = 0
+    for i in range(len(data)):
+        avg = beta*z + (1 - beta)*data[i]
+        bc = avg/(1 - beta**(i+1))
+        ema.append(bc)
+    return ema
