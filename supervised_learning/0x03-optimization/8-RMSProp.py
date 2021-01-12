5e07f21311233886c7b420d075fe0dd23b0e1fc6
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-12 05:34:13 -0800

    8

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Normalization function """
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """ creates the training operation for a neural network """
+    return tf.train.RMSPropOptimizer(alpha,beta2, epsilon=epsilon).minimize(loss)
