aaf62ce44aa7206e1ddf25a4ad2fb259f11d13b3
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-14 08:59:30 -0800

    Adam

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -6,6 +6,11 @@ import numpy as np
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """ creates the training operation for a neural network """
-    s = s * beta2 + (1 - beta2) * (grad ** 2)
-    var = var - alpha * grad / (np.sqrt(s) + epsilon)
-    return var, s
+    v = beta1 * v + (1 - beta1) * grad
+    v_corrected = v / (1 - beta1 ** t)
+
+    s = beta2 * s  + (1 - beta2) * (grad ** 2)
+    s_corrected = s / (1 - beta2 ** t)
+
+    var = var - alpha * v_corrected / (np.sqrt(s_corrected) + epsilon)
+    return var, v, s
