10fdc123860bd226a3f853d02d59d18f5329005a
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-24 12:41:29 -0800

    remove square

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -8,5 +8,5 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """ Calculates the cost of a neural network with L2 regularization """
     sum = 0
     for i in range(1, L+1):
-        sum += np.linalg.norm(weights["W" + str(i)]) ** 2
+        sum += np.linalg.norm(weights["W" + str(i)])
     return cost + (lambtha / (2 * m)) * sum
