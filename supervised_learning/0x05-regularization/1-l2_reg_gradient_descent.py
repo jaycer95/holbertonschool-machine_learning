20fdc738bac861f75373ee7efa4569fcbf792cec
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-24 13:00:29 -0800

    Reg grad desc

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -8,11 +8,12 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """Updates the weights and biases of a neural network """
     m = Y.shape[1]
     dz = cache["A" + str(L)] - Y
-    for i in range(L, 0 , -1):
+    for i in range(L, 0, -1):
         dw = np.matmul(dz, cache["A" + str(i - 1)].T) / m
-        db = np.sum(dz, axis = 1, keepdims=True) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
         dA = 1 - cache["A" + str(i - 1)] * cache["A" + str(i - 1)]
         dz = np.matmul(weights["W" + str(i)].T, dz) * dA
 
-        weights["W" + str(i)] = weights["W" + str(i)] - alpha * (dw + weights["W" + str(i)]) * (lambtha / m)
+        weights["W" + str(i)] = weights["W" + str(i)] - \
+            alpha * (dw + weights["W" + str(i)]) * (lambtha / m)
         weights["b" + str(i)] = weights["W" + str(i)] - alpha * db
