4f2a75d5237b0a67806f6732c67ccf8e832d1da2
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-24 12:39:43 -0800

    cost

---
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Regularization """
+
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """ Calculates the cost of a neural network with L2 regularization """
+    sum = 0
+    for i in range(1, L+1):
+        sum += np.linalg.norm(weights["W" + str(i)]) ** 2
+    return cost + (lambtha / (2 * m)) * sum
