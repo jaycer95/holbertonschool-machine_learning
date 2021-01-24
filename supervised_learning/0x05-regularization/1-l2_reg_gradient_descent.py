b410bae5f3043d1db4496de8a944e326850b5335
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-24 13:02:15 -0800

    Reg grad desc

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -7,13 +7,16 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """Updates the weights and biases of a neural network """
     m = Y.shape[1]
-    dz = cache["A" + str(L)] - Y
-    for i in range(L, 0, -1):
-        dw = np.matmul(dz, cache["A" + str(i - 1)].T) / m
-        db = np.sum(dz, axis=1, keepdims=True) / m
-        dA = 1 - cache["A" + str(i - 1)] * cache["A" + str(i - 1)]
-        dz = np.matmul(weights["W" + str(i)].T, dz) * dA
+    dZ = cache["A" + str(L)] - Y
+    for j in range(L, 0, -1):
+        Act = cache["A" + str(j-1)]
+        dW = np.matmul(dZ, Act.T) / m
+        db = np.sum(dZ, axis=1, keepdims=True) / m
+        tanh = 1 - Act*Act
+        dZ = np.matmul(weights[
+            "W" + str(j)].T, dZ) * tanh
 
-        weights["W" + str(i)] = weights["W" + str(i)] - \
-            alpha * (dw + weights["W" + str(i)]) * (lambtha / m)
-        weights["b" + str(i)] = weights["W" + str(i)] - alpha * db
+        dW_L2reg = dW + (lambtha/m)*weights["W" + str(j)]
+
+        weights["W" + str(j)] -= alpha * dW_L2reg
+        weights["b" + str(j)] -= alpha * db
