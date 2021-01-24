4d1c685bd8bc67e48a91deac8a915b7567ae7226
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-24 13:37:54 -0800

    dropout

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -6,9 +6,10 @@ import numpy as np
 
 def dropout_forward_prop(X, weights, L, keep_prob):
     """Conducts forward propagation using Dropout"""
-    cache = {"A0" : X}
+    cache = {"A0": X}
     for i in range(L):
-        z = np.matmul(weights["W" + str(i + 1)], cache["A" + str(i)]) + weights["b" + str(i + 1)]
+        z = np.matmul(weights["W" + str(i + 1)],
+                      cache["A" + str(i)]) + weights["b" + str(i + 1)]
         drop = np.random.binomial(1, keep_prob, size=z.shape)
         if i == L - 1:
             x = np.exp(z)
@@ -17,6 +18,6 @@ def dropout_forward_prop(X, weights, L, keep_prob):
             cache["A" + str(i + 1)] = np.tanh(z)
             cache["D" + str(i + 1)] = drop
             cache["A" + str(i + 1)] = (cache["A" + str(i + 1)]
-                                        ) * cache[
-                                            "D" + str(i + 1)] / keep_prob
+                                       ) * cache[
+                "D" + str(i + 1)] / keep_prob
     return cache
