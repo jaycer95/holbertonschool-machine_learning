462b866ce769f9ee578d11387893988b18cffba3
jaycer95 <jacer.dabbabi95@gmail.com>
2021-01-28 13:40:35 -0800

    train

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Keras """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels,
+                batch_size, epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, verbose=True, shuffle=False):
+    """ train the model using early stopping """
+    callback = None
+    if early_stopping:
+        callback = K.callbacks.EarlyStopping(patience=patience)
+    history = network.fit(x=data, y=labels, callbacks=[callback],
+                          epochs=epochs, batch_size=batch_size,
+                          validation_data=validation_data,
+                          verbose=verbose, shuffle=shuffle)
+    return history
