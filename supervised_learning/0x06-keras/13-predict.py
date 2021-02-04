1c4c63ebf42b46fe85a4ac2bcafb1e433a90f1c8
jaycer95 <jacer.dabbabi95@gmail.com>
2021-02-04 14:16:53 -0800

    13

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env python3
+""" Keras """
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """make a prediction using a neural network"""
+    return network.predict(data, verbose=verbose)
