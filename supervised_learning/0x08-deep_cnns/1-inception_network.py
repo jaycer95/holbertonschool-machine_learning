f1745f872ba242d82f92c425a0e62b3e15e92316
jaycer95 <jacer.dabbabi95@gmail.com>
2021-03-08 06:43:46 -0800

    1

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Inception Network """
-from tensorflow import keras as K
+import tensorflow.keras as K
 inception_block = __import__('0-inception_block').inception_block
 
 
