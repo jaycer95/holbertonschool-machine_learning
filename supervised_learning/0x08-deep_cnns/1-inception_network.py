b437a6a2c8bf9286d6cf965c0b3f4787f1ed531d
jaycer95 <jacer.dabbabi95@gmail.com>
2021-03-08 05:41:03 -0800

    Test

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -5,49 +5,30 @@ inception_block = __import__('0-inception_block').inception_block
 
 
 def inception_network():
-    """function that builds the inception network as described
-    in Going Deeper with Convolutions (2014)"""
-    # create an input model with shape=(224, 224, 3)
-    X = K.Input(shape=(224, 224, 3))
-    # create an output of the inception network
-    Z = K.layers.Conv2D(filters=64, kernel_size=(7, 7), strides=2,
-                        padding='same', activation='relu')(X)
-
-    Z = K.layers.MaxPool2D(pool_size=(3, 3),
-                           strides=2, padding='same')(Z)
-    Z = K.layers.Conv2D(filters=64, kernel_size=(1, 1),
-                        strides=1, padding='same',
-                        activation='relu')(Z)
-    Z = K.layers.Conv2D(
-        filters=192,
-        kernel_size=(
-            3,
-            3),
-        strides=1,
-        padding='same',
-        activation='relu')(Z)
-    Z = K.layers.MaxPool2D(pool_size=(3, 3),
-                           strides=2, padding='same')(Z)
-    Z = inception_block(Z, [64, 96, 128, 16, 32, 32])
-    Z = inception_block(Z, [128, 128, 192, 32, 96, 64])
-    Z = K.layers.MaxPool2D(pool_size=(3, 3),
-                           strides=2, padding='same')(Z)
-
-    Z = inception_block(Z, [192, 96, 208, 16, 48, 64])
-    Z = inception_block(Z, [160, 112, 224, 24, 64, 64])
-    Z = inception_block(Z, [128, 128, 256, 24, 64, 64])
-    Z = inception_block(Z, [112, 144, 288, 32, 64, 64])
-    Z = inception_block(Z, [256, 160, 320, 32, 128, 128])
-
-    Z = K.layers.MaxPool2D(pool_size=(3, 3),
-                           strides=(2, 2), padding='same')(Z)
-    Z = inception_block(Z, [256, 160, 320, 32, 128, 128])
-    Z = inception_block(Z, [384, 192, 384, 48, 128, 128])
-    Z = K.layers.AveragePooling2D(pool_size=(7, 7), strides=1,
-                                  padding='valid')(Z)
-
-    Z = K.layers.Dropout(0.4)(Z)
-    Y = K.layers.Dense(units=1000, activation='softmax')(Z)
-    # a keras model frome the input and the outputs after the inception block
-    model = K.models.Model(inputs=X, outputs=Y)
+    """ Build the inception network """
+    input_layer = K.Input(shape=(224, 224, 3))
+    layer = K.layers.Conv2D(64, (7, 7), strides=2, padding='same', activation='relu')(input_layer)
+    layer = K.layers.MaxPooling2D(
+        (3, 3), strides=2, padding='same')(layer)
+    layer = K.layers.Conv2D(64, (3, 3), strides=1, padding='same', activation='relu')(layer)
+    layer = K.layers.Conv2D(192, (3, 3), strides=1, padding='same', activation='relu')(layer)
+    layer = K.layers.MaxPooling2D(
+        (3, 3), strides=2, padding='same')(layer)
+    layer = inception_block(layer, (64, 96, 128, 16, 32, 32))
+    layer = inception_block(layer, (128, 128, 192, 32, 96, 64))
+    layer = K.layers.MaxPooling2D(
+        (3, 3), strides=2, padding='same')(layer)
+    layer = inception_block(layer, (192, 96, 208, 16, 48, 64))
+    layer = inception_block(layer, (160, 112, 224, 24, 64, 64))
+    layer = inception_block(layer, (128, 128, 256, 24, 64, 64))
+    layer = inception_block(layer, (112, 144, 288, 32, 64, 64))
+    layer = inception_block(layer, (256, 160, 320, 32, 128, 128))
+    layer = K.layers.MaxPooling2D(
+        (3, 3), strides=2, padding='same')(layer)
+    layer = inception_block(layer, (256, 160, 320, 32, 128, 128))
+    layer = inception_block(layer, (384, 192, 384, 48, 128, 128))
+    layer = K.layers.AveragePooling2D((7, 7), strides=1)(layer)
+    layer = K.layers.Dropout(0.4)(layer)
+    layer = K.layers.Dense(1000, activation='softmax')(layer)
+    model = K.models.Model(inputs=input_layer, outputs=layer)
     return model
