dbb34f0773a22db7d6b1a2e4e4bb3c7432bc91d6
jaycer95 <jacer.dabbabi95@gmail.com>
2021-03-08 05:53:54 -0800

    Test

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -48,26 +48,9 @@ def inception_network():
         strides=2,
         padding='same')(layer)
 
-    layer = inception_block(layer, (64, 96, 128, 16, 32, 32))
+    layer = inception_block(layer, [64, 96, 128, 16, 32, 32])
 
-    layer = inception_block(layer, (128, 128, 192, 32, 96, 64))
-
-    layer = K.layers.MaxPooling2D(
-        pool_size=(
-            3,
-            3),
-        strides=2,
-        padding='same')(layer)
-
-    layer = inception_block(layer, (192, 96, 208, 16, 48, 64))
-
-    layer = inception_block(layer, (160, 112, 224, 24, 64, 64))
-
-    layer = inception_block(layer, (128, 128, 256, 24, 64, 64))
-
-    layer = inception_block(layer, (112, 144, 288, 32, 64, 64))
-
-    layer = inception_block(layer, (256, 160, 320, 32, 128, 128))
+    layer = inception_block(layer, [128, 128, 192, 32, 96, 64])
 
     layer = K.layers.MaxPooling2D(
         pool_size=(
@@ -76,9 +59,26 @@ def inception_network():
         strides=2,
         padding='same')(layer)
 
-    layer = inception_block(layer, (256, 160, 320, 32, 128, 128))
+    layer = inception_block(layer, [192, 96, 208, 16, 48, 64])
 
-    layer = inception_block(layer, (384, 192, 384, 48, 128, 128))
+    layer = inception_block(layer, [160, 112, 224, 24, 64, 64])
+
+    layer = inception_block(layer, [128, 128, 256, 24, 64, 64])
+
+    layer = inception_block(layer, [112, 144, 288, 32, 64, 64])
+
+    layer = inception_block(layer, [256, 160, 320, 32, 128, 128])
+
+    layer = K.layers.MaxPooling2D(
+        pool_size=(
+            3,
+            3),
+        strides=2,
+        padding='same')(layer)
+
+    layer = inception_block(layer, [256, 160, 320, 32, 128, 128])
+
+    layer = inception_block(layer, [384, 192, 384, 48, 128, 128])
 
     layer = K.layers.AveragePooling2D(pool_size=(
         7, 7), strides=1, padding='valid')(layer)
