b645a63c5026b4b5fe5d52e553ba966145563c0f
jaycer95 <jacer.dabbabi95@gmail.com>
2021-03-08 07:06:14 -0800

    2

---
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Identity Block """
+import tensorflow.keras as K
+
+
+def identity_block(A_prev, filters):
+    """ Build an identity block """
+    initializer = K.initializers.he_normal()
+    layer = K.layers.Conv2D(
+        filters[0],
+        (1,
+         1),
+        padding='same',
+        activation='relu',
+        kernel_initializer=initializer)(A_prev)
+    layer = K.layers.BatchNormalization()(layer)
+    layer = K.layers.Activation('relu')(layer)
+    layer = K.layers.Conv2D(
+        filters[1],
+        (3,
+         3),
+        padding='same',
+        activation='relu',
+        kernel_initializer=initializer)(layer)
+    layer = K.layers.BatchNormalization()(layer)
+    layer = K.layers.Activation('relu')(layer)
+    layer = K.layers.Conv2D(
+        filters[2],
+        (1,
+         1),
+        padding='same',
+        activation='relu',
+        kernel_initializer=initializer)(layer)
+    layer = K.layers.BatchNormalization()(layer)
+    output = K.layers.Add()([layer, A_prev])
+    output = K.layers.Activation('relu')(output)
+    return output
