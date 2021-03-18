6f6b998ddeee6af6bc81134bb61571f46ed1bffc
jaycer95 <jacer.dabbabi95@gmail.com>
2021-03-18 04:05:22 -0700

    determinant

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" advanced linear algebra """
+
+
+def determinant(matrix):
+    """ Determinant of a matrix """
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError('matrix must be a list of lists')
+    if any(not isinstance(i, list) for i in matrix):
+        raise TypeError('matrix must be a list of lists')
+    if matrix == [[]]:
+        return 1
+    if any(len(i) != len(matrix) for i in matrix):
+        raise ValueError('matrix must be a square matrix')
+    n = len(matrix)
+    if matrix == [[]]:
+        return 1
+    if n == 1:
+        return matrix[0][0]
+    else:
+        det = 0
+        for i in range(n):
+            extract = []
+            for j in range(1, n):
+                row = []
+                for k in range(n):
+                    if k != i:
+                        row.append(matrix[j][k])
+                extract.append(row)
+            det += (-1) ** i * matrix[0][i] * determinant(extract)
+    return det
