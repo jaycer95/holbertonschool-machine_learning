f0cc578847bc005e42a1ccc5c40b924846fab0c5
jaycer95 <jacer.dabbabi95@gmail.com>
2021-03-22 15:27:29 -0700

    check

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -10,9 +10,9 @@ class MultiNormal:
         """class constructor"""
         if not isinstance(data, np.ndarray) or data.ndim != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        d = data.shape[0]
-        if d < 2:
+        n = data.shape[1]
+        if n < 2:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1, keepdims=True)
         arg_mean = data - self.mean
-        self.cov = (np.dot(arg_mean, arg_mean.T)) / (d - 1)
+        self.cov = (np.dot(arg_mean, arg_mean.T)) / (n - 1)
