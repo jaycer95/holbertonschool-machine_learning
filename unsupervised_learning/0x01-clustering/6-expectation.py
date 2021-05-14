439c85c89b95478c8fb073b8d27acbbddfbc806b
jaycer95 <jacer.dabbabi95@gmail.com>
2021-05-14 12:57:03 +0100

    6

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
-"""
-Function that calculates the expectation step in the EM
-algorithm for a GMM
-"""
+""" Expectation """
 import numpy as np
 pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
-    """
-    Returns: g, l, or None, None on failure
-    g is a numpy.ndarray of shape (k, n) containing the
-    posterior probabilities for each data point in each cluster
-    l is the total log likelihood
-    """
+    """ Calculate the expectation step in the EM algorithm for a GMM """
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None
     if type(m) is not np.ndarray or len(m.shape) != 2:
@@ -31,7 +23,6 @@ def expectation(X, pi, m, S):
     if not np.isclose(np.sum(pi), 1):
         return None, None
 
-    n, _ = X.shape
     k = pi.shape[0]
     P = [pdf(X, m[i], S[i]) * pi[i] for i in range(k)]
     g = P / np.sum(P, axis=0)
