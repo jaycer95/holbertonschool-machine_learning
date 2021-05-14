d8721633b3f2b05fd02a417507a0cf5dfb1531d3
jaycer95 <jacer.dabbabi95@gmail.com>
2021-05-14 20:15:54 +0100

    check

--- unsupervised_learning/0x02-hmm/0-markov_chain.py
+++ unsupervised_learning/0x02-hmm/0-markov_chain.py
@@ -5,7 +5,8 @@ import numpy as np
 
 
 def markov_chain(P, s, t=1):
-    """ Determine the probability of a markov chain being in a particular state after a specified number of iterations """ 
+    """ Determine the probability of a markov chain being
+    in a particular state after a specified number of iterations """
     if type(P) is not np.ndarray or len(P.shape) != 2:
         return None
     n = P.shape[1]
@@ -14,5 +15,5 @@ def markov_chain(P, s, t=1):
     if np.any(np.sum(P, axis=1)) != 1:
         return None
     for _ in range(t):
-        s = np.matmul(s,P)
+        s = np.matmul(s, P)
     return s
