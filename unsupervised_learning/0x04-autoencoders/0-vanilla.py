047e074909017aee0141041f6f534da14b45629e
jaycer95 <jacer.dabbabi95@gmail.com>
2021-05-31 15:05:54 +0100

    Vanilla Autoencoder

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -7,13 +7,16 @@ import tensorflow.keras as keras
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """ Create an autoencoder """
     encoder_input = keras.Input(shape=(input_dims,))
-    encoded = keras.layers.Dense(hidden_layers[0], activation='relu')(encoder_input)
+    encoded = keras.layers.Dense(
+        hidden_layers[0],
+        activation='relu')(encoder_input)
     for i in hidden_layers[1::]:
         encoded = keras.layers.Dense(i, activation='relu')(encoded)
     latent = keras.layers.Dense(latent_dims, activation='relu')(encoded)
     encoder = keras.Model(encoder_input, latent)
     decoder_input = keras.Input(shape=(latent_dims,))
-    decoded = keras.layers.Dense(hidden_layers[-1], activation='relu')(decoder_input)
+    decoded = keras.layers.Dense(
+        hidden_layers[-1], activation='relu')(decoder_input)
     for i in hidden_layers[-2::-1]:
         decoded = keras.layers.Dense(i, activation='relu')(decoded)
     decoded = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
