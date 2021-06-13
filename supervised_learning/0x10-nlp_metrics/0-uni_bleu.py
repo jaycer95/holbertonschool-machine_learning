1071dbc75ee012f366aba017e2e8b0f8f636df42
jaycer95 <jacer.dabbabi95@gmail.com>
2021-06-13 20:08:17 +0100

    Unigram BLEU

---
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" NLP - Evaluation Metrics """
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """ Calculate the unigram BLEU score for a sentence"""
+    c = len(sentence)
+    refslen = np.array([len(r) for r in references])
+    refminidx = np.argmin(np.abs(refslen - c))
+    r = len(references[refminidx])
+    if r > c:
+        bp = np.exp(1 - r / c)
+    else:
+        bp = 1    
+    words = dict()
+    for word in sentence:
+        for ref in references:
+            if word in words:
+                if words[word] < ref.count(word):
+                    words.update({word: ref.count(word)})
+            else:
+                words.update({word: ref.count(word)})
+    p = sum(words.values())
+    return bp * p / c
