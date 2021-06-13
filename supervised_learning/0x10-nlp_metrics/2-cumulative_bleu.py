a1008ed05a977a056032c3f665fdcdec12cbffe9
jaycer95 <jacer.dabbabi95@gmail.com>
2021-06-13 20:40:00 +0100

    Cumulative BLEU

---
+++ supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+""" NLP - Evaluation Metrics """
+import numpy as np
+
+
+def ngrams(sentences, n):
+    """ ngrams sentence """
+    ngrams_sentence = []
+    for i in range(len(sentences) - n + 1):
+        ngrams_sentence.append(' '.join(sentences[i:i + n]))
+    return ngrams_sentence
+
+
+def ngram_bleu(references, sentence, n):
+    """ Calculate the n-gram BLEU score for a sentence """
+    c = len(sentence)
+    refslen = np.array([len(r) for r in references])
+    refminidx = np.argmin(np.abs(refslen - c))
+    r = len(references[refminidx])
+    if r > c:
+        bp = np.exp(1 - r / c)
+    else:
+        bp = 1
+    s = ngrams(sentence, n)
+    r = list(ngrams(ref, n) for ref in references)
+    words = dict()
+    for word in s:
+        for ref in r:
+            if word in words:
+                if words[word] < ref.count(word):
+                    words.update({word: ref.count(word)})
+            else:
+                words.update({word: ref.count(word)})
+    p = sum(words.values())
+    return bp * p / len(s)
+
+
+def cumulative_bleu(references, sentence, n):
+    """ Calculate the cumulative n-gram BLEU score for a sentence """
+    c = len(sentence)
+    refslen = np.array([len(r) for r in references])
+    refminidx = np.argmin(np.abs(refslen - c))
+    r = len(references[refminidx])
+    if r > c:
+        bp = np.exp(1 - r / c)
+    else:
+        bp = 1
+    ngrams = []
+    for i in range(1, n + 1):
+        ngrams.append(ngram_bleu(references, sentence, i))
+    ngrams = np.array(ngrams)
+    score = np.exp(np.sum((1 / n) * np.log(ngrams)))
+    return bp * score
