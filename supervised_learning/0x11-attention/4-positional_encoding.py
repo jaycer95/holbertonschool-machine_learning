f0ee06a0f6a52d79b3f8a2fa9ee6fefa14eb859b
jaycer95 <jacer.dabbabi95@gmail.com>
2021-06-14 15:51:27 +0100

    4

---
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Attention """
+import numpy as np
+
+
+def positional_encoding(max_seq_len, dm):
+    """ Calculate the positional encoding for a transformer"""
+    positional_embeddings = np.zeros((max_seq_len, dm))
+    for position in range(max_seq_len):
+        for i in range(0, dm, 2):
+            positional_embeddings[position, i] = np.sin(position / np.power(10000 ,  (2*i) // np.float(dm))) 
+                                                    
+            positional_embeddings[position, i + 1] = np.cos(position / np.power(10000 , ( 2 * (i + 1) ) // np.float(dm)))
+    return positional_embeddings
