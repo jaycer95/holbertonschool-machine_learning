8bd1e6e3a4e43d9db060efbe8bd12485af3feabe
jaycer95 <jacer.dabbabi95@gmail.com>
2021-06-14 16:35:44 +0100

    Multi Head Attention

---
+++ supervised_learning/0x11-attention/6-multihead_attention.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Attention """
+import tensorflow as tf
+sdp_attention = __import__('5-sdp_attention').sdp_attention
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """ Perform multi head attention """
+    def __init__(self, dm, h):
+        """initialization"""
+        super(MultiHeadAttention, self).__init__()
+        self.dm = dm
+        self.h = h
+        self.depth = dm // h
+        self.Wq = tf.keras.layers.Dense(dm)
+        self.Wk = tf.keras.layers.Dense(dm)
+        self.Wv = tf.keras.layers.Dense(dm)
+        self.linear = tf.keras.layers.Dense(dm)
+
+    def call(self, Q, K, V, mask):
+        """call function  """
+        batch_size = tf.shape(Q)[0]
+
+        q = self.Wq(Q)
+        q = tf.reshape(q, (batch_size, -1, self.h, self.depth))
+        q = tf.transpose(q, perm=[0, 2, 1, 3])
+
+        k = self.Wk(K)
+        k = tf.reshape(k, (batch_size, -1, self.h, self.depth))
+        k = tf.transpose(k, perm=[0, 2, 1, 3])
+
+        v = self.Wv(V)
+        v = tf.reshape(v, (batch_size, -1, self.h, self.depth))
+        v = tf.transpose(v, perm=[0, 2, 1, 3])
+
+        sdpsoftmax, sdpoutput = sdp_attention(
+            q, k, v, mask)
+        sdpsoftmax = tf.transpose(sdpsoftmax, perm=[0, 2, 1, 3])
+
+        concat = tf.reshape(sdpsoftmax, (batch_size, -1, self.dm))
+
+        output = self.linear(concat)
+
+        return output, sdpoutput
